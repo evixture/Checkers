bc15d41e5b9eb422ea7ae0f2a59774c017fc2699
Andrew <pixture1311@gmail.com>
2024-09-27 19:52:06 -0500

    still trying

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ enum Piece {
 
 struct Board {
     boardArr: [[Piece; 8]; 8], //2d 8x8 array
-                            //there is probably a much more simple representation of a checkers board
-                            //since half of the board cannot be used and pieces can only move diagonally
+    //there is probably a much more simple representation of a checkers board
+    //since half of the board cannot be used and pieces can only move diagonally
 }
 
 impl Board {
@@ -19,18 +19,18 @@ impl Board {
             for x in &self.boardArr[y].len() {
                 if (x % 2 == 0) && (y[x] % 2 == 1) {
                     if y[x] <= 2 {
-                       &self.boardArr[y][x] = Piece::Black;
+                        &self.boardArr[y][x] = Piece::Black;
                     } else if y[x] >= 5 {
-                       &self.boardArr[y][x] = Piece::Red;
+                        &self.boardArr[y][x] = Piece::Red;
                     }
                 } else if (x % 2 == 1) && (y[x] % 2 == 0) {
                     if y[x] <= 2 {
-                       &self.boardArr[y][x] = Piece::Black;
+                        &self.boardArr[y][x] = Piece::Black;
                     } else if y[x] >= 5 {
-                       &self.boardArr[y][x] = Piece::Red;
+                        &self.boardArr[y][x] = Piece::Red;
                     }
                 } else {
-                   &self.boardArr[y][x] = Piece::None;
+                    &self.boardArr[y][x] = Piece::None;
                 }
             }
         }
