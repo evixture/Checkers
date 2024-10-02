faa868056e4e142d837c81aada905f8d2109bda8
evixture <pixture1311@gmail.com>
2024-10-02 08:37:54 -0500

    using iced instead of tauri

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,40 @@
-//use std::collections::LinkedList;
-use tauri;
+use iced::widget::{button, column, text, Column};
+use iced::Center;
+
+#[derive(Default)]
+struct Counter {
+    value: i64,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    Increment,
+    Decrement,
+}
+
+impl Counter {
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::Increment => {
+                self.value += 1;
+            }
+            Message::Decrement => {
+                self.value -= 1;
+            }
+        }
+    }
+
+    fn view(&self) -> Column<Message> {
+        column![
+            button("Increment").on_press(Message::Increment),
+            text(self.value).size(50),
+            button("Decrement").on_press(Message::Decrement)
+        ]
+        .padding(20)
+        .align_x(Center)
+    }
+}
+
 enum Piece {
     None,
     Red,
@@ -12,9 +47,7 @@ fn two_to_one(&x: &usize, &y: &usize) -> usize {
 }
 
 struct Board {
-    board_arr: Vec<Piece>, //2d 8x8 array
-                           //there is probably a much more simple representation of a checkers board
-                           //since half of the board cannot be used and pieces can only move diagonally
+    board_arr: Vec<Piece>,
 }
 
 impl Board {
@@ -23,33 +56,11 @@ impl Board {
         //new_var.set_len(64 : usize);
         for y in 0..8 {
             for x in 0..8 {
-                // if (x % 2 == 0) && (y % 2 == 1) {
-                //     if y <= 2 {
-                //         //new_var[two_to_one(&x, &y)] = Piece::Black;
-                //         new_var.push(Piece::Black);
-                //     } else if y >= 5 {
-                //         //new_var[two_to_one(&x, &y)] = Piece::Red;
-                //         new_var.push(Piece::Red);
-                //     }
-                // } else if (x % 2 == 1) && (y % 2 == 0) {
-                //     if y <= 2 {
-                //         //new_var[two_to_one(&x, &y)] = Piece::Black;
-                //         new_var.push(Piece::Black);
-                //     } else if y >= 5 {
-                //         //new_var[two_to_one(&x, &y)] = Piece::Red;
-                //         new_var.push(Piece::Red);
-                //     }
-                // } else {
-                //     //new_var[two_to_one(&x, &y)] = Piece::None;
-                //     new_var.push(Piece::None);
-                // }
-
                 if (y == 0 || y == 2) && (x % 2 == 0) {
                     new_var.push(Piece::Black);
                 } else if (y == 5 || y == 7) && (x % 2 == 1) {
                     new_var.push(Piece::Red);
                 } else if (y == 1) && (x % 2 != 0) {
-                    //problem >BBBBXXXX
                     new_var.push(Piece::Black);
                 } else if (y == 6) && (x % 2 == 0) {
                     new_var.push(Piece::Red);
@@ -58,7 +69,7 @@ impl Board {
                 }
             }
         }
-        println!("size of board: {}", new_var.len());
+        //println!("size of board: {}", new_var.len());
         Board { board_arr: new_var }
     }
 
@@ -78,7 +89,9 @@ impl Board {
     }
 }
 
-fn main() {
+fn main() -> iced::Result {
     let b: Board = Board::new();
     b.print();
+
+    iced::run("Hello, world!", Counter::update, Counter::view)
 }
