6958722e33ed011f3c7b43192c1f43db15d693f9
evixture <pixture1311@gmail.com>
2024-10-07 08:44:48 -0500

    need to learn more about rust

--- src/board.rs
+++ src/board.rs
@@ -1,14 +1,24 @@
 use crate::window::Message;
-use iced::widget::{button, column, text, Column, Row};
+use iced;
+//use iced::widget::Button;
+use iced::widget::button;
+use iced::widget::column;
+use iced::widget::text;
+use iced::widget::Column;
+use iced::widget::Row;
 use iced::Element;
 
+#[derive(Default)]
 enum Piece {
+    #[default]
     None,
     Red,
     Black,
 }
 
-enum MoveMessage {
+#[derive(Clone, Debug, Default)]
+pub(crate) enum MoveMessage {
+    #[default]
     Selection(i32, i32),
 }
 
@@ -17,15 +27,16 @@ fn two_to_one(&x: &usize, &y: &usize) -> usize {
     (y * 8) + x
 }
 
+#[derive(Debug, Default)]
 pub struct Board {
     board_arr: Vec<Piece>,
-    board_view: iced::widget::Column<MoveMessage>,
+    board_view: Column<'static, MoveMessage>,
 }
 
 impl Board {
     pub fn new() -> Self {
         let mut new_board_arr: Vec<Piece> = Vec::with_capacity(64);
-        let mut new_board_view = iced::widget::Column::new();
+        let mut new_board_vec: Vec<Element<MoveMessage>> = Vec::with_capacity(64);
 
         //new_var.set_len(64 : usize);
         for y in 0..8 {
@@ -43,16 +54,17 @@ impl Board {
                 } else {
                     new_board_arr.push(Piece::None);
                 }
-                row.push(iced::widget::Button("something"));
+                //use iced::widget::button;
+                row.push(button("something").into());
                 //Row::push(row, iced::widget::Button("something")); //.push(iced::widget::Button("something"));
                 //row.push(iced::widget::Button("something"));
             }
-            new_board_view.push(iced::widget::Row::from_vec(row));
+            new_board_vec.push(Row::from_vec(row).into());
         }
         //println!("size of board: {}", new_var.len());
         Board {
             board_arr: new_board_arr,
-            board_view: new_board_view,
+            board_view: Column::from_vec(new_board_vec),
         }
     }
 
@@ -71,5 +83,9 @@ impl Board {
         }
     }
 
-    pub fn get_view() -> iced::Element<'_, MoveMessage> {}
+    pub fn update(&self) {}
+
+    pub fn get_view(self) -> Element<'static, MoveMessage> {
+        self.board_view.into()
+    }
 }
--- src/window.rs
+++ src/window.rs
@@ -1,9 +1,11 @@
+use crate::board::{Board, MoveMessage};
 use iced::widget::{button, column, text, Column};
 use iced::Center;
 
 #[derive(Default)]
 pub struct CheckersWindow {
-    value: i64,
+    //value: i64,
+    board: Board,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -13,20 +15,27 @@ pub enum Message {
 }
 
 impl CheckersWindow {
-    //iced::run expects a state and message from update function
-    pub fn update(&mut self, message: Message) {
-        match message {
-            Message::Increment => {
-                self.value += 1;
-            }
-            Message::Decrement => {
-                self.value -= 1;
-            }
+    pub fn new(self) -> Self {
+        CheckersWindow {
+            board: Board::new(),
         }
     }
 
+    //iced::run expects a state and message from update function
+    pub fn update(&mut self, message: MoveMessage) {
+        // match message {
+        //     Message::Increment => {
+        //         self.value += 1;
+        //     }
+        //     Message::Decrement => {
+        //         self.value -= 1;
+        //     }
+        // }
+        self.board.update();
+    }
+
     //iced::run expects at least a state from view function
-    pub fn view(&self) -> iced::Element<'_, Message> {
+    pub fn view(self) -> iced::Element<'static, MoveMessage> {
         // iced::widget::column![
         //     button("Increment").on_press(Message::Increment),
         //     text(self.value).size(50),
@@ -35,6 +44,7 @@ impl CheckersWindow {
         // .padding(20)
         // .align_x(Center)
 
-        iced::widget::container(iced::widget::row![button("Red")]).into()
+        //iced::widget::container(iced::widget::row![button("Red")]).into()
+        self.board.get_view()
     }
 }
