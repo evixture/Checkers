adb45085f9fe72a97f9f648b38e92870c971fd20
Andrew <pixture1311@gmail.com>
2024-10-13 19:07:31 -0500

    got spaces to be different colors

--- src/window.rs
+++ src/window.rs
@@ -1,6 +1,10 @@
 use crate::board::{map2d, piece_to_string, Board, MoveMessage, Piece};
+use iced::event::Status;
+use iced::theme::Palette;
+use iced::widget::button::Style;
 use iced::widget::{button, Button, Container, Row};
-use iced::Element;
+use iced::{color, theme, Background, Border, Color, Element, Length, Shadow, Theme};
+use std::ffi::CString;
 // iced::widget::button;
 
 // #[derive(Default)]
@@ -28,24 +32,47 @@ use iced::Element;
 //     }
 // }
 
-pub fn update(board: &mut Board, msg: MoveMessage) {}
+pub fn update(_board: &mut Board, _msg: MoveMessage) {}
 
 pub fn view(board: &Board) -> Element<MoveMessage> {
     let mut col: Vec<iced::Element<MoveMessage>> = Vec::with_capacity(Board::WIDTH);
-    let mut v: Vec<iced::Element<MoveMessage>> = Vec::with_capacity(Board::WIDTH);
-
     for y in 0..Board::WIDTH {
+        let mut v: Vec<iced::Element<MoveMessage>> = Vec::with_capacity(Board::WIDTH);
         for x in 0..Board::WIDTH {
-            let str: String;
-            match board.board_arr[map2d(&x, &y, &Board::WIDTH)] {
-                Piece::None => str = "X".to_string(),
-                Piece::Red => str = "R".to_string(),
-                Piece::Black => str = "B".to_string(),
-            };
-            v.push(Button::new("X").into());
-            //r.push(button(piece_to_string()));
+            v.push(
+                Button::new(match board.board_arr[map2d(&x, &y, &Board::WIDTH)] {
+                    Piece::None => "",
+                    Piece::Red => "R",
+                    Piece::Black => "B",
+                })
+                .width(100)
+                .height(100)
+                .style(if (y + x) % 2 == 0 {
+                    style_white
+                } else {
+                    style_black
+                })
+                .into(),
+            );
         }
         col.push(iced::widget::Row::from_vec(v).into());
     }
     iced::widget::Column::from_vec(col).into()
+}
+
+fn style_white(t: &Theme, s: button::Status) -> Style {
+    Style {
+        background: Option::from(Background::Color(color!(216, 183, 159))),
+        text_color: Color::WHITE,
+        border: Border::default(),
+        shadow: Shadow::default(),
+    }
+}
+fn style_black(t: &Theme, s: button::Status) -> Style {
+    Style {
+        background: Option::from(Background::Color(color!(97, 61, 51))),
+        text_color: Color::BLACK,
+        border: Border::default(),
+        shadow: Shadow::default(),
+    }
 }
